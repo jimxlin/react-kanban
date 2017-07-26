7939245ad2cc4ef99278a6f2eb58e32c217e693f
Jim X. Lin <miljinx@gmail.com>
2017-07-25 22:13:43 -0700

    Add state to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const boards = {
+  todo: [],
+  doing: [],
+  done: []
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boards,
+      cardCount: 0
+    };
+  }
   render() {
     return (
       <div className="App">
